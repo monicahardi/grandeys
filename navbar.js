4cce355fa823d8ba0a96530c5c9e099efc76f5e1
monicahardi <122967318+monicahardi@users.noreply.github.com>
2023-02-10 18:41:13 +0700

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,17 @@
+// Nama : Monica Hardi
+// NIM : 10122335
+// Kelas : IF-9
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
